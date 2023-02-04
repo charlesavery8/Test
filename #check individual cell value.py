cc6d5c6a36dd032744e688b5b0a54ddf09c65004
charlesavery8 <120284357+charlesavery8@users.noreply.github.com>
2023-02-04 15:20:30 -0600

    Update #check individual cell value.py

--- #check individual cell value.py
+++ #check individual cell value.py
@@ -6,7 +6,7 @@ from openpyxl import load_workbook
 hw2 = '.\code\INFS247HW2.xlsx'
 wb = load_workbook(hw2)
 
-emp = wb["Employees"]
+Sheet_emp = wb["Employees"]
 cell = 'F8'
-check = emp[cell]
+check = Sheet_emp[cell]
 print(check.value)
